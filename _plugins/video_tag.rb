155abc04c2fc302255ec90d6a86ff061e4f02564
Andrew Eick <maeick@missionfocus.com>
2014-08-20 15:23:48 -0400

    stable and non-responsive

--- _plugins/video_tag.rb
+++ _plugins/video_tag.rb
@@ -1,14 +1,25 @@
 module Jekyll
-	class Vimeo < Liquid::Tag
-	  def initialize(tag_name, video_id, tokens)
-	    super
-	    @video_id = video_id
-	  end
+  class Vimeo < Liquid::Tag
+    def initialize(tag_name, video_id, tokens)
+      super
+      @video_id = video_id
+    end
 
-	  def render(context)
-	    "<figure><div class='video'><iframe src='//player.vimeo.com/video/#{@video_id}' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div></figure>"
-	  end
-	end
+    def render(context)
+      picturefillDiv = ERB.new <<-VIDEOJSTEMPLATE.gsub(/^ {8}/,'')
+        <video 
+          id="vid1" 
+          src="" 
+          class="video-js vjs-default-skin" controls preload="auto" 
+          width="640" height="360" 
+          data-setup='{ "techOrder": ["vimeo"], "src": "https://vimeo.com/<%= @video_id %>", "loop": true, "autoplay": false }'
+        >
+          <p>Video Playback Not Supported</p>
+        </video>
+      VIDEOJSTEMPLATE
+      return picturefillDiv.result(binding)
+    end
+  end
 end
 
 Liquid::Template.register_tag('video', Jekyll::Vimeo)
